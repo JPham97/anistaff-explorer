b59cbd4778b1b6c0fbffa60191d0837ae42dfc3c
Jeremy Pham <jeremypham97@gmail.com>
2020-10-09 00:41:14 -0700

    add query & directives for userlist but no VA mapping yet

--- fetcherAL.py
+++ fetcherAL.py
@@ -31,84 +31,125 @@ fragment titleCharVA on Media {
     }
 }
 
-query ($id: Int, $page: Int) {
-    Media(id: $id, type: ANIME) {
+query ($id: Int, $page: Int, $doList: Boolean!, $user: String) {
+    Media(id: $id, type: ANIME) @skip(if: $doList) {
         ...titleCharVA
+    }
+    MediaListCollection(userName: $user, type: ANIME, status: COMPLETED, forceSingleCompletedList: true) @include(if: $doList) {
+  	    lists {
+  	        name
+  	        status
+            entries {
+                progress
+                # media {
+                #     ...titleCharVA
+                # }
+            }
+  	    }
     }
 }
 '''
 url = 'https://graphql.anilist.co'
 
-# default page values (this is the usual case, having 25+ chars is rare)
-currPage = 1
-lastPage = 1
+# given an id for a show on anilist, return a pair (title, dictionary)
+# output dictionary contains mappings for the shows VAs -> chars
+def getAniVAs(id):
 
-numChars = 0
-actorToChar = {}
+    # default page values (this is the usual case, having 25+ chars is rare)
+    currPage = 1
+    lastPage = 1
 
-# while there are still characters to be fetched
-while currPage <= lastPage:
+    numChars = 0
+    actorToChar = {}
 
-    # inside the loop since the page can change
+    # while there are still characters to be fetched
+    while currPage <= lastPage:
+
+        # inside the loop since the page can change
+        variables = {
+            'id': id,
+            'page': currPage,
+            'doList': False
+        }
+
+        # Make the HTTP Api request
+        print(f"Making a query for page {currPage}")
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        response = json.loads(response.text)
+
+        # response is converted to JSON at this point
+        # process the response and convert to VA -> char map
+
+        anime = response['data']['Media']
+        title = anime['title']['romaji']
+
+        # use pageInfo to determine if another query needs to be made since there
+        # are too many characters (25+)
+        #
+        # pageInfo is a dict with keys
+        # 'total' => total number of items
+        # 'perPage'
+        # 'currentPage'
+        # 'lastPage'
+        # 'hasNextPage'
+        info = anime['characters']['pageInfo']
+
+        # reassign these so the loop runs again if needed
+        currPage = info['currentPage']
+        lastPage = info['lastPage']
+
+        # a single edge connects a char node to a list of voice actors
+        # edge is a dict with keys 'node' and 'voiceActors'
+        edges = anime['characters']['edges']
+        for edge in edges:
+            charName = edge['node']['name']['full']
+            voice_actors = edge['voiceActors']
+
+            numChars += 1
+
+            for actor in voice_actors:
+                nameVA = f"{actor['name']['last']}, {actor['name']['first']}"
+                actorToChar.setdefault(nameVA, [])
+                actorToChar[nameVA].append(charName)
+
+        # pprint.pprint(actorToChar)
+        # print(len(actorToChar))
+
+        print(f"{title} has {info['total']} chars, {numChars} found")
+        print(f"currently on page {info['currentPage']} of {info['lastPage']}")
+
+        # if incrementing this exceeds 'lastPage' then loop terminates
+        # otherwise, it will make another query for subsequent pages
+        currPage += 1
+
+    pprint.pprint(actorToChar)
+    return (title, actorToChar)
+
+# implement a query to retrieve a user's Completed List
+# userName -> a user with an anime list on AniList
+# outputs a
+#
+def getVAsFromList(userName):
+    # query variables
     variables = {
-        #'id': 113813 # Kanokari, 8 chars
-        #'id': 104454, # isekai quartet, 43 chars
-        'id': 20992, # haikyuu 2, 35 chars
-        'page': currPage,
+        'user': userName,
+        'doList': True
     }
 
     # Make the HTTP Api request
-    print(f"Making a query for page {currPage}")
+    print(f"Making a query for user {userName}")
     response = requests.post(url, json={'query': query, 'variables': variables})
     response = json.loads(response.text)
+    return response
 
-    # response is converted to JSON at this point
-    # process the response and convert to VA -> char map
+#id: 113813 # Kanokari, 8 chars
+#id: 104454, # isekai quartet, 43 chars, 40 VAs (3 VAs have 2 roles)
+#id: 20992, haikyuu2, 35 chars
 
-    anime = response['data']['Media']
-    title = anime['title']['romaji']
+# id = 20992
+# (aniTitle, seiyuus) = getAniVAs(id)
+# print(f'\n{aniTitle} has {len(seiyuus)} voice actors\n')
 
-    # use pageInfo to determine if another query needs to be made since there
-    # are too many characters (25+)
-    #
-    # pageInfo is a dict with keys
-    # 'total' => total number of items
-    # 'perPage'
-    # 'currentPage'
-    # 'lastPage'
-    # 'hasNextPage'
-    info = anime['characters']['pageInfo']
+data = getVAsFromList("Swagocytosis")
 
-    # reassign these so the loop runs again if needed
-    currPage = info['currentPage']
-    lastPage = info['lastPage']
-
-
-
-    # a single edge connects a char node to a list of voice actors
-    # edge is a dict with keys 'node' and 'voiceActors'
-    edges = anime['characters']['edges']
-    for edge in edges:
-        charName = edge['node']['name']['full']
-        voice_actors = edge['voiceActors']
-
-        numChars += 1
-
-        for actor in voice_actors:
-            nameVA = f"{actor['name']['last']}, {actor['name']['first']}"
-            actorToChar.setdefault(nameVA, [])
-            actorToChar[nameVA].append(charName)
-
-    # pprint.pprint(actorToChar)
-    # print(len(actorToChar))
-
-    print(f"{title} has {info['total']} chars, {numChars} found")
-    print(f"currently on page {info['currentPage']} of {info['lastPage']}")
-
-    # if incrementing this exceeds 'lastPage' then loop terminates
-    # otherwise, it will make another query for subsequent pages
-    currPage += 1
-
-pprint.pprint(actorToChar)
-
-# implement a query to retrieve a user's Completed List
+print(data)
